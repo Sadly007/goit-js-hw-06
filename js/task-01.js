d766d1505fa070ce74245abf1d4417bce5972247
Sadly007 <romankisel89@gmail.com>
2022-09-10 20:13:31 +0300

    =

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const navEl = document.querySelectorAll('.item');
+// console.log(navEl)
+const numberOfCategories = navEl.length
+console.log("Number of categories:", numberOfCategories)
+
+const tagsH = document.querySelector('.hero-title-one');
+console.log('Category:', tagsH.textContent)
+
+const value = do
